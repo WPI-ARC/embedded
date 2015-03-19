f3006643b3ea0484da5093c110007cb51388801d
Riley Larkins <rdlarkins@wpi.edu>
2015-03-19 08:57:57 -0400

    Working control from the paper.

--- finger_control_module/main.cpp
+++ finger_control_module/main.cpp
@@ -29,7 +29,8 @@ AnalogIn topStretchEgain(PTB10); //P0_14
 AnalogIn midStretchEgain(PTB3); //P0_16
 AnalogIn botStretchEgain(PTB2); //P0_12
 
-DigitalOut solenoid(PTA2); //P0_9
+// DigitalOut solenoid(PTA2); //P0_9
+PwmOut solenoid(PTA2);
 
 Serial pc(USBTX, USBRX);
 
@@ -40,15 +41,15 @@ Ticker incrementer;
 Timer timer;
 
 /***** Constant Declarations *****/
-static const int POSITION_LENGTH = 7;
-static const float POSITION_THRESHOLDS[7] = { 0.6742,  0.5878, 0.3682,  0.3275,  0.2403,  0.1640,  0.0};
-static const float POSITION_SLOPES[7]     = {19.6842, 13.3633, 9.5637, 20.6475, 14.4391, 33.0176, 82.7497};
-static const float POSITION_OFFSETS[7]    = { 3.3881,  7.6499, 9.8833,  5.8019,  7.8355,  3.3713, -4.7829};
+static const int POSITION_LENGTH = 4;
+static const float POSITION_THRESHOLDS[4] = {0.2618, 0.2142,  0.1905, 0.0};
+static const float POSITION_SLOPES[4]     = {0.2166, 0.6304,  4.2187, 0.8922};
+static const float POSITION_OFFSETS[4]    = {0.2433, 0.1349, -0.6338, 0.0};
 
-static const int PRESSURE_LENGTH = 5;
-static const float PRESSURE_THRESHOLDS[5] = {0.3191,  0.2937, 0.2494,  0.2046, 0.0};
-static const float PRESSURE_SLOPES[5]     = {1.6105,  2.9458, 1.6951,  2.7928, 1.2196};
-static const float PRESSURE_OFFSETS[5]    = {0.1610, -0.2651, 0.1022, -0.1716, 0.1503};
+static const int PRESSURE_LENGTH = 2;
+static const float PRESSURE_THRESHOLDS[2] = { 0.1956,  0.1744};
+static const float PRESSURE_SLOPES[2]     = { 1.0429,  9.4116};
+static const float PRESSURE_OFFSETS[2]    = {-0.0040, -1.6410};
 
 /***** Variable Declarations *****/
 float weightp = 0, weightf = 0;
@@ -60,6 +61,7 @@ float f_pterm = 0, f_iterm = 0, f_dterm = 0;
 float forceterm = 0;
 float forceff = 0, f_positionff = 0;
 
+float stretch0 = 0, stretch1 = 0, stretch2 = 0;
 float actualp = 0, desiredp = 0, errorp = 0, l_errorp = 0;
 float p_kp_p, p_ki_p, p_kd_p;
 float p_kp_n, p_ki_n, p_kd_n;
@@ -67,6 +69,7 @@ float p_pterm = 0, p_iterm = 0, p_dterm = 0;
 float positionterm = 0;
 float p_positionff = 0;
 
+float forcelimiter = 0;
 float pressure = 0;
 
 float actualpre = 0, desiredpre = 0, errorpre = 0, l_errorpre = 0;
@@ -87,15 +90,29 @@ int contact = 0;
 
 /***** Main *****/
 int main(void) {
+    solenoid.period(0.02);
+    solenoid.write(0.0);
     init();
+    pc.baud(115200);
     
     fcontroller.attach(&computeControl, TIMESTEP);
-    pwm.attach(&pwmout, 0.0002);
+    // pwm.attach(&pwmout, 0.0002);
     incrementer.attach(&increment, 0.1);
     timer.start();
     
     while(1) {
-        pc.printf("%i, %f, %f, %f, %f, %f, %f, %f, %f, %f\r\n", timer.read_ms(), actualf, desiredf, actualp, desiredp, dutycycle, forceterm, positionterm, weightf, weightp);
+        // calibrate pressure and position stepping dutycycle
+        // pc.printf("%i, %f, %f, %f, %f, %f\r\n", timer.read_ms(), dutycycle, actualpre, stretch0, stretch1, stretch2);
+        // pc.printf("%i, %f, %f, %f, %f\r\n", timer.read_ms(), dutycycle, actualpre, actualp, actualf);
+
+        // debugging force
+        // pc.printf("%i, %f, %f, %f, %f, %f, %f, %f, %f, %f\r\n", timer.read_ms(), actualf, desiredf, dutycycle, f_pterm, f_iterm, f_dterm, f_positionff, actualp, actualpre);
+
+        // debuging prius
+        // pc.printf("%i, %f, %f, %f, %f, %f, %f, %f\r\n", timer.read_ms(), actualf, actualp, desiredp, positionterm, p_positionff, f_positionff, forcelimiter);
+
+        // standard print
+        pc.printf("%i, %f, %f, %f, %f, %f, %f, %f\r\n", timer.read_ms(), actualf, desiredf, actualp, desiredp, dutycycle, forceterm, positionterm);
     }
 }
 
@@ -103,18 +120,18 @@ int main(void) {
 void init(void) {
     f_kp_p = 80;
     f_ki_p = 90;
-    f_kd_p = 0;
+    f_kd_p = 0.5;
     
     f_kp_n = 25;
     f_ki_n = 5;
-    f_kd_n = 0;
+    f_kd_n = 0.5;
 
-    p_kp_p = 50;
-    p_ki_p = 25;
+    p_kp_p = 75;
+    p_ki_p = 100;
     p_kd_p = 0;
 
     p_kp_n = 75;
-    p_ki_n = 75;
+    p_ki_n = 175;
     p_kd_n = 0;
     
     pre_kp = 5; 
@@ -129,7 +146,7 @@ float computeWeight(float force, float max_force, float position, float position
     cap(0, &x_norm, 1);
 
     float power = (-15) * (0.5 + (x_norm / 2) - y_norm);
-    return (1 / (1 + pow(EULER, power)));
+    return (1 / (1 + pow(exp(1), power)));
 }
 
 void computeControl(void) {
@@ -137,9 +154,19 @@ void computeControl(void) {
     actualf = topForceEgain.read();
     l_errorf = errorf;
     errorf = desiredf - actualf;
+
+    if(fabs(errorf) > 0.05) {
+        f_iterm = 0;
+    }
+    if(fabs(errorp) > 0.05) {
+        p_iterm = 0;
+    }
     
     cap(0.2, &desiredp, 1.0);
-    position[filter_index] = (botStretchEgain.read() + midStretchEgain.read() + topStretchEgain.read()) / 3;
+    stretch0 = botStretchEgain.read();
+    stretch1 = midStretchEgain.read();
+    stretch2 = topStretchEgain.read();
+    position[filter_index] = (stretch0 + stretch1)/2; // + stretch2)/3; top is dead again...
     filter_index++;
     if(filter_index == 5)
         filter_index = 0;
@@ -147,23 +174,14 @@ void computeControl(void) {
     l_errorp = errorp;
     errorp = desiredp - actualp;
 
-    weightp = computeWeight(actualf, desiredf, actualp, desiredp);
-    weightf = 1 - weightp;
-
     /***** FORCE CONTROL *****/
     if(errorf >= 0) {
         f_pterm = f_kp_p * errorf;
-        if(fabs(errorf) < 0.075)
-            f_iterm = (f_iterm * ALPHA) + (f_ki_p * errorf * TIMESTEP);
-        else
-            f_iterm = (f_iterm * ALPHA);
+        f_iterm = (f_iterm * ALPHA) + (f_ki_p * errorf * TIMESTEP);
         f_dterm = f_kd_p * (errorf - l_errorf) / TIMESTEP;
     } else {
         f_pterm = f_kp_n * errorf;
-        if(fabs(errorf) < 0.05)
-            f_iterm = (f_iterm * ALPHA) + (f_ki_n * errorf * TIMESTEP);
-        else
-            f_iterm = (f_iterm * ALPHA);
+        f_iterm = (f_iterm * ALPHA) + (f_ki_n * errorf * TIMESTEP);
         f_dterm = f_kd_n * (errorf - l_errorf) / TIMESTEP;
     }
         
@@ -174,11 +192,12 @@ void computeControl(void) {
     forceff = (FSLOPE * desiredf) + FOFFSET;
     for(int i = 0; i < POSITION_LENGTH; ++i) {
         if(actualp > POSITION_THRESHOLDS[i]) {
-            f_positionff = (POSITION_SLOPES[i] * actualp) + POSITION_OFFSETS[i];
+            f_positionff = ((POSITION_SLOPES[i] * actualp) + POSITION_OFFSETS[i] - 0.1619)/0.0122;
             break;
         }
     }
     forceterm = f_pterm + f_iterm + f_dterm + forceff + f_positionff;
+    cap(-25.0, &forceterm, 25);
 
     /***** POSITION CONTROL *****/
     if(errorp >= 0) {
@@ -197,16 +216,20 @@ void computeControl(void) {
 
     for(int i = 0; i < POSITION_LENGTH; ++i) {
         if(desiredp > POSITION_THRESHOLDS[i]) {
-            p_positionff = (POSITION_SLOPES[i] * desiredp) + POSITION_OFFSETS[i];
+            p_positionff = ((POSITION_SLOPES[i] * desiredp) + POSITION_OFFSETS[i] - 0.1619)/0.0122;
             break;
         }
     }
 
     positionterm = p_pterm + p_iterm + p_dterm + p_positionff;
+    cap(-25.0, &positionterm, 25);
     
     /***** COMBINE TERMS *****/
-    //TODO: Add weights
-    pressure = (weightf * forceterm) + (weightp* positionterm);
+    float power = -6.0 * errorf;
+    forcelimiter = 1 - pow(EULER, power);
+    cap(0.0, &forcelimiter, 1.0);
+
+    pressure = fmin(positionterm, forceterm); //(positionterm * forcelimiter) + ((f_positionff + (FSLOPE*desiredf+FOFFSET)) * 0.95 * (1 - forcelimiter));
 
     cap(0.0, &pressure, 25.0);
     
@@ -233,6 +256,8 @@ void computeControl(void) {
     
     dutycycle = pre_pterm + pre_iterm + pre_dterm + pressureff;
     cap(0.0, &dutycycle, 1.0);
+
+    solenoid.write(dutycycle);
 }
 
 void pwmout(void) {
@@ -248,52 +273,68 @@ void pwmout(void) {
 }
 
 void increment(void) {
-    // if(actualf > CONTACT) {
-        // if(step < 400) {
-        //     desiredf = ((float)((int)(step/40)))/10.0;
-        // } else if(step < 800) {
-        //     desiredf = ((float)((int)((800-step)/40)))/10.0;
-        // } else if(step < 1000) {
-        //     desiredf += 0.005;
-        // } else if(step < 1200) {
-        //     desiredf -= 0.005;
-        // } else if(step < 1300) {
-        //     desiredf = 0.3;
-        // } else if(step < 1400) {
-        //     desiredf = 0.8;
-        // } else if(step < 1500) {
-        //     desiredf = 0.3;
-        // } else {
-        //     desiredf = 0.1;
-        // }
-        // step++;
+    // calibrate pressure & position
+    // if(step < 10000) {
+    //     if(((step / 1000) % 2) == 0) {
+    //         dutycycle = ((float)(step % 1000) / 1000.0) * 0.15 + 0.2;
+    //     } else {
+    //         dutycycle = ((1000 - (float)(step % 1000)) / 1000.0) * 0.15 + 0.2;
+    //     }
+    // } else {
+    //     dutycycle = 0;
     // }
 
-    desiredf = 0.8;
-    if(step < 50) {
-        desiredp = 0.2;
-    } else if(step < 250) {
-        desiredp = (((float)((int)((step-50)/40)))/10.0) + 0.2;
-    } else if(step < 450) {
-        desiredp = 0.6 - (((float)((int)((step-250)/40)))/10.0);
-    } else if(step < 500) {
-        desiredp = 0.2;
-    } else if(step < 600) {
-        desiredp += 0.0035;
-    } else if(step < 700) {
-        desiredp -= 0.0035;
-    } else if(step < 750) {
-        desiredp = 0.25;
-    } else if(step < 850) {
-        desiredp = 0.4;
-    } else if(step < 950) {
-        desiredp = 0.6;
-    } else if(step < 1050) {
-        desiredp = 0.4;
-    } else {
-        desiredp = 0.25;
-    }
-    step++;
+    // if(actualf > CONTACT) {
+    //     if(step < 440) {
+    //         desiredf = ((float)((int)(step/40)))/10.0;
+    //     } else if(step < 840) {
+    //         desiredf = ((float)((int)((840-step)/40)))/10.0;
+    //     } else if(step < 1000) {
+    //         desiredf += 0.005;
+    //     } else if(step < 1200) {
+    //         desiredf -= 0.005;
+    //     } else if(step < 1300) {
+    //         desiredf = 0.3;
+    //     } else if(step < 1400) {
+    //         desiredf = 0.8;
+    //     } else if(step < 1500) {
+    //         desiredf = 0.3;
+    //     } else {
+    //         desiredf = 0.1;
+    //     }
+    //     step++;
+    // }
+
+    desiredf = 0.1;
+    desiredp = 0.35;
+    // if(step < 50) {
+    //     desiredp = 0.25;
+    // } else if(step < 690) {
+    //     desiredp = (((float)((int)((step-50)/80)))/20.0) + 0.25;
+    // } else if(step < 1330) {
+    //     desiredp = 0.6 - (((float)((int)((step-690)/80)))/20.0);
+    // } else if(step < 1400) {
+    //     desiredp = 0.25;
+    // } else if(step < 1533) {
+    //     desiredp += 0.0030;
+    // } else if(step < 1666) {
+    //     desiredp -= 0.0030;
+    // } else if(step < 1800) {
+    //     desiredp = 0.25;
+    // } else if(step < 1900) {
+    //     desiredp = 0.40;
+    // } else if(step < 2000) {
+    //     desiredp = 0.55;
+    // } else if(step < 2100) {
+    //     desiredp = 0.70;
+    // } else if(step < 2200) {
+    //     desiredp = 0.55;
+    // } else if(step < 2300) {
+    //     desiredp = 0.40;
+    // } else {
+    //     desiredp = 0.25;
+    // }
+    // step++;
 
     cap(0.20, &desiredp, 1.0);
     cap(0.1, &desiredf, 1.0);
